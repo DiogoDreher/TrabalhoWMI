44a5e18ea9638628d72ab52a8d6b0ab458987e71
DiogoDreher <45463068+DiogoDreher@users.noreply.github.com>
2018-12-02 20:29:16 +0000

    Add files via upload

---
+++ Js/slider.js
@@ -0,0 +1,12 @@
+<!-- jQuery library - Please load it from Google API's -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js" type="text/jаvascript"></script>
+<!-- jQuery UI Widget and Effects Core (custom download)
+     You can make your own at: http://jqueryui.com/download -->
+<script src="js/jquery-ui-1.8.23.custom.min.js" type="text/jаvascript"></script>
+<!-- Latest version (3.0.6) of jQuery Mouse Wheel by Brandon Aaron
+     You will find it here: http://brandonaaron.net/code/mousewheel/demos -->
+<script src="js/jquery.mousewheel.min.js" type="text/jаvascript"></script>
+<!-- jQuery Kinectic (1.5) used for touch scrolling -->
+<script src="js/jquery.kinetic.js" type="text/jаvascript"></script>
+<!-- Smooth Div Scroll 1.3 minified-->
+<script src="js/jquery.smoothdivscroll-1.3-min.js" type="text/jаvascript"></script>
